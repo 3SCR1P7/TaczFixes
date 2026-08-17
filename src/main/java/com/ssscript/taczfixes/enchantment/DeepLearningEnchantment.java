188e96d98684ffc5a0372f187e61fb7dcf8db2f6
3SCR1P7 <3303788247@qq.com>
2026-08-17 20:13:35 +0800

    Add files via upload

--- src/main/java/com/ssscript/taczfixes/enchantment/DeepLearningEnchantment.java
+++ src/main/java/com/ssscript/taczfixes/enchantment/DeepLearningEnchantment.java
@@ -1,46 +1,9 @@
 package com.ssscript.taczfixes.enchantment;
 
 import com.ssscript.taczfixes.Config;
-import com.ssscript.taczfixes.util.GunEnchantmentHelper;
-import com.tacz.guns.api.item.IGun;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
-public class DeepLearningEnchantment extends Enchantment {
+public class DeepLearningEnchantment extends BaseGunEnchantment {
     public DeepLearningEnchantment() {
-        super(Rarity.VERY_RARE, EnchantmentCategory.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
-    }
-
-    @Override
-    public int getMaxLevel() {
-        return Config.ENCH_DEEP_LEARNING_MAX_LEVEL.get();
-    }
-
-    @Override
-    public int getMinCost(int level) {
-        return 5 + level * 5;
-    }
-
-    @Override
-    public int getMaxCost(int level) {
-        return getMinCost(level) + level * 4;
-    }
-
-    @Override
-    public boolean isTreasureOnly() {
-        return true;
-    }
-
-    @Override
-    public boolean canApplyAtEnchantingTable(ItemStack stack) {
-        if (!GunEnchantmentHelper.isEnabled()) {
-            return false;
-        }
-        if (!(stack.getItem() instanceof IGun)) {
-            return false;
-        }
-        return GunEnchantmentHelper.isEnchantAllowed(this);
+        super(Rarity.VERY_RARE, Config.ENCH_DEEP_LEARNING_MAX_LEVEL::get, 5, 5, 4);
     }
 }
--- src/main/java/com/ssscript/taczfixes/enchantment/ElectromagneticCoilEnchantment.java
+++ src/main/java/com/ssscript/taczfixes/enchantment/ElectromagneticCoilEnchantment.java
@@ -1,46 +1,9 @@
 package com.ssscript.taczfixes.enchantment;
 
 import com.ssscript.taczfixes.Config;
-import com.ssscript.taczfixes.util.GunEnchantmentHelper;
-import com.tacz.guns.api.item.IGun;
-import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
-public class ElectromagneticCoilEnchantment extends Enchantment {
+public class ElectromagneticCoilEnchantment extends BaseGunEnchantment {
     public ElectromagneticCoilEnchantment() {
-        super(Rarity.RARE, EnchantmentCategory.WEAPON, new EquipmentSlot[]{EquipmentSlot.MAINHAND});
-    }
-
-    @Override
-    public int getMaxLevel() {
-        return Config.ENCH_COIL_MAX_LEVEL.get();
-    }
-
-    @Override
-    public int getMinCost(int level) {
-        return 6 + level * 8;
-    }
-
-    @Override
-    public int getMaxCost(int level) {
-        return getMinCost(level) + level * 6;
-    }
-
-    @Override
-    public boolean isTreasureOnly() {
-        return true;
-    }
-
-    @Override
-    public boolean canApplyAtEnchantingTable(ItemStack stack) {
-        if (!GunEnchantmentHelper.isEnabled()) {
-            return false;
-        }
-        if (!(stack.getItem() instanceof IGun)) {
-            return false;
-        }
-        return GunEnchantmentHelper.isEnchantAllowed(this);
+        super(Rarity.RARE, Config.ENCH_COIL_MAX_LEVEL::get, 6, 8, 6);
     }
 }
