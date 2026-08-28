9b55b468e582c367332f220520ff9ab473d6193f
3SCR1P7 <3303788247@qq.com>
2026-08-28 17:03:07 +0800

    Add files via upload

---
+++ src/main/java/com/ssscript/taczfixes/common/accessor/EntityKineticBulletAccessor.java
@@ -0,0 +1,15 @@
+package com.ssscript.taczfixes.common.accessor;
+
+import com.tacz.guns.entity.EntityKineticBullet;
+import net.minecraft.resources.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(EntityKineticBullet.class)
+public interface EntityKineticBulletAccessor {
+    @Accessor(value = "shotDamageMultiplier", remap = false)
+    float taczfixes$getShotDamageMultiplier();
+
+    @Accessor(value = "ammoId", remap = false)
+    void taczfixes$setAmmoId(ResourceLocation ammoId);
+}
