9dd7c4a67c16dbba0d49888f61be928b38e2851d
3SCR1P7 <3303788247@qq.com>
2026-09-05 19:42:01 +0800

    Update example_logic.lua

--- example/example_logic.lua
+++ example/example_logic.lua
@@ -1,10 +1,12 @@
 local M = {}
 
 function M.shoot(api)
+    api:runCommand("scoreboard objectives add tacz_time dummy")
     local level = api:getLevel()
-    if level >= 100 then
+    if level >= 100 and api:getScoreboardValue("tacz_time") <= 10 then
         local amount = tostring(level)
         api:runCommand("give @s minecraft:diamond",amount)
+        api:runCommand("scoreboard players add @s tacz_time 1")
         api:setExp(api:getExp() - 1 )
     end
 end
